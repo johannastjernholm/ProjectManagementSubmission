7a5a9525d791a966d676043e1771a2d53ea31094
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-02-10 14:56:27 +0100

    Add DataContext and migrations for database

---
+++ Data/DataContexts/DataContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.DataContexts;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    //Reg alla entiteter
+    public DbSet<CustomerEntity> Customers { get; set; }
+    public DbSet<ProductEntity> Products { get; set; }
+    public DbSet<ProjectEntity> Projects { get; set; }
+    public DbSet<StatusTypeEntity> StatusTypes { get; set; }
+    public DbSet<UserEntity> Users { get; set; }
+
+}
---
+++ Data/DataContexts/DataContextFactory.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.DataContexts;
+
+public class DataContextFacory : IDesignTimeDbContextFactory<DataContext>
+{
+    public DataContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
+        // Connectionstring
+        optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\ProjectManagementSubmission\Data\DataBases\local_pm_database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
+
+        return new DataContext(optionsBuilder.Options);
+    }
+}
