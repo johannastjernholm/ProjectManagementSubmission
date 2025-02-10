f518cf60f0102f36ce6179c414e05cdde41e9e0c
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-02-10 14:29:55 +0100

    Add entities

---
+++ Data/Entities/CustomerEntity.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class CustomerEntity
+{
+    [Key]
+    public int Id { get; set; }
+    [Column(TypeName = "nvarchar(100)")]
+    public string CustomerName { get; set; } = null!;
+}
---
+++ Data/Entities/StatusTypeEntity.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Data.Entities;
+
+public class StatusTypeEntity
+{
+    [Key]
+    public int Id { get; set; }
+    [Column(TypeName = "nvarchar(60)")]
+    public string StatusName { get; set; } = null!;
+}
