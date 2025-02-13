01255eda2c281359690d808656ab88d7825f6723
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-02-13 15:03:33 +0100

    Add Repositories

---
+++ Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,50 @@
+﻿using Data.DataContexts;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public class CustomerRepository(DataContext context)
+{
+
+    //Create
+    public CustomerEntity CreateCustomer(CustomerEntity customerEntity)
+    {
+        context.Customers.Add(customerEntity);
+        context.SaveChanges();
+
+        return customerEntity;
+    }
+
+    //Read
+    public IEnumerable<CustomerEntity> GetAllCustomers()
+    {
+        return context.Customers.ToList();
+    }
+
+    //Update
+    public CustomerEntity UpdateCustomer(CustomerEntity customerEntity)
+    {
+        context.Customers.Update(customerEntity);
+        context.SaveChanges();
+        return customerEntity;
+    }
+
+
+    //Delete
+    public async Task<bool>  DeleteAsync(int id)
+    {
+        var entity = await context.Customers.FirstOrDefaultAsync(x => x.Id == id);
+        if (entity != null)
+        {
+            context.Customers.Remove(entity);
+            await context.SaveChangesAsync();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
---
+++ Data/Repositories/ProjectRepository.cs
@@ -0,0 +1,47 @@
+﻿using Data.DataContexts;
+using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data.Repositories;
+
+public class ProjectRepository(DataContext context)
+{
+    //Create
+    public ProjectEntity CreateProject(ProjectEntity projectEntity)
+    {
+        context.Projects.Add(projectEntity);
+        context.SaveChanges();
+
+        return projectEntity;
+    }
+
+    //Read
+    public IEnumerable<ProjectEntity> GetProjects()
+    {
+        return context.Projects.ToList();
+    }
+
+    //Update
+    public ProjectEntity UpdateProject(ProjectEntity projectEntity)
+    {
+        context.Projects.Update(projectEntity);
+        context.SaveChanges();
+        return projectEntity;
+    }
+
+    //Delete
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var entity = await context.Projects.FirstOrDefaultAsync(x => x.Id == id);
+        if (entity != null)
+        {
+            context.Projects.Remove(entity);
+            await context.SaveChangesAsync();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
