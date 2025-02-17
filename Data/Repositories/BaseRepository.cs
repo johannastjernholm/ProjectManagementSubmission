e0c5b8b30c2a169b1befefbc14a3577c6f8226f5
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-02-17 15:58:45 +0100

    Add Base repository

---
+++ Data/Repositories/BaseRepository.cs
@@ -0,0 +1,52 @@
+﻿using Data.DataContexts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Data.Repositories;
+//Base repositoryn hanterar funktionalitet nära databasen
+public abstract class BaseRepository<TEntity>(DataContext context) where TEntity : class
+{
+    protected readonly DataContext _context = context;
+    protected readonly DbSet<TEntity> _db = context.Set<TEntity>();
+
+    //Lägga till ny entitet
+    public async Task AddAsync(TEntity entity)
+    {
+        await _db.AddAsync(entity);
+        await _context.SaveChangesAsync();
+    }
+    //Hämta läsabar lista över alla
+    public async Task<IEnumerable<TEntity>> GetAsync()
+    {
+        var entities = await _db.ToListAsync();
+        return entities;
+    }
+    //Hämta en särskild entitet
+    public async Task<TEntity?> GetAsync(Expression<Func<TEntity, bool>> expression)
+    {
+        var entity = await _db.FirstOrDefaultAsync(expression);
+        return entity;
+    }
+    //Uppdatera entitet
+    public async Task UpdateAsync(TEntity entity)
+    {
+        _db.Update(entity);
+        await _context.SaveChangesAsync();
+
+    }
+    //Radera entitet
+    public async Task RemoveAsync(TEntity entity)
+    {
+        _db.Remove(entity);
+        await _context.SaveChangesAsync();
+    }
+
+
+
+
+
+
+
+
+
+}
--- Data/Repositories/ProjectRepository.cs
+++ Data/Repositories/ProjectRepository.cs
@@ -1,47 +1,9 @@
-﻿using Data.DataContexts;
+﻿
+using Data.DataContexts;
 using Data.Entities;
-using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories;
 
-public class ProjectRepository(DataContext context)
+public class ProjectRepository(DataContext context) : BaseRepository<ProjectEntity>(context)
 {
-    //Create
-    public ProjectEntity CreateProject(ProjectEntity projectEntity)
-    {
-        context.Projects.Add(projectEntity);
-        context.SaveChanges();
-
-        return projectEntity;
-    }
-
-    //Read
-    public IEnumerable<ProjectEntity> GetProjects()
-    {
-        return context.Projects.ToList();
-    }
-
-    //Update
-    public ProjectEntity UpdateProject(ProjectEntity projectEntity)
-    {
-        context.Projects.Update(projectEntity);
-        context.SaveChanges();
-        return projectEntity;
-    }
-
-    //Delete
-    public async Task<bool> DeleteAsync(int id)
-    {
-        var entity = await context.Projects.FirstOrDefaultAsync(x => x.Id == id);
-        if (entity != null)
-        {
-            context.Projects.Remove(entity);
-            await context.SaveChangesAsync();
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
 }
