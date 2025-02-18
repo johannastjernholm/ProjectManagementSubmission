8087b5ea32a36412396df2c72b773a8d12ffa25d
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-02-18 12:00:47 +0100

    Add Project models

---
+++ Business/Models/Project.cs
@@ -0,0 +1,12 @@
+﻿namespace Business.Models;
+
+public class Project
+{
+    public int Id { get; set; }
+    public string Description { get; set; } = null!;
+    public string? Notes { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string Status { get; set; } = null!;
+    public int CustomerId { get; set; }
+}
---
+++ Business/Models/ProjectRegistrationForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Business.Models;
+
+public class ProjectRegistrationForm
+{
+    public string Description { get; set; } = null!;
+    public string? Notes { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+    public string Status { get; set; } = null!;
+    public string CustomerName { get; set; } = null!;
+}
