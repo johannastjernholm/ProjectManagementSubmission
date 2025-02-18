da9a7d7302b40afc915c29aa971997026d332164
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-02-18 13:00:54 +0100

    Create Project factory

--- Business/Factories/CustomerFactory.cs
+++ Business/Factories/CustomerFactory.cs
@@ -2,7 +2,7 @@
 using Data.Entities;
 
 namespace Business.Factories;
-
+//Koden skriven vid Hans Mattin-Lassei föreläsning
 public static class CustomerFactory
 {
     public static CustomerEntity? Create(CustomerRegistrationForm form) => form == null ? null : new()
---
+++ Business/Factories/ProjectFactory.cs
@@ -0,0 +1,31 @@
+﻿using Business.Models;
+using Data.Entities;
+
+namespace Business.Factories;
+//Koden skriven med hjälp av ChatGPT
+
+public static class ProjectFactory
+{
+    //Skapa entitet av datan vi får in av användaren
+    public static ProjectEntity? Create(ProjectRegistrationForm form) => form == null ? null : new()
+    {
+        Description = form.Description,
+        Notes = form.Notes,
+        StartDate = form.StartDate,
+        EndDate = form.EndDate,
+        Status = form.Status,
+        CustomerId = 0
+    };
+
+
+    public static Project? Create(ProjectEntity entity) => entity == null ? null : new()
+    { 
+        Id = entity.Id,
+        Description = entity.Description,
+        Notes = entity.Notes,
+        StartDate = entity.StartDate,
+        EndDate = entity.EndDate,
+        Status = entity.Status,
+        CustomerId = entity.CustomerId,     
+    };
+}
