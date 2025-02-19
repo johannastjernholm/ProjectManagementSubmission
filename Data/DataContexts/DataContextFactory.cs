1c456be98dc606ff8d99367b0d775026ae253201
Johanna Stjernholm <jo.stjernholm@gmail.com>
2025-02-19 11:37:16 +0100

    Fix: connection string

--- Data/DataContexts/DataContextFactory.cs
+++ Data/DataContexts/DataContextFactory.cs
@@ -12,7 +12,7 @@ public class DataContextFacory : IDesignTimeDbContextFactory<DataContext>
     {
         var optionsBuilder = new DbContextOptionsBuilder<DataContext>();
         // Connectionstring
-        optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\ProjectManagementSubmission\Data\DataBases\local_pm_database.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True");
+        optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projects\ProjectManagementSubmission\Data\DataBases\local_pm_database.mdf;Initial Catalog=local_pm_database;Integrated Security=True;Connect Timeout=30;Encrypt=True");
 
         return new DataContext(optionsBuilder.Options);
     }
